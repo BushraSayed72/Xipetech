e7dab20d93ab72405527aa18209fdfaaaaf1da28
BushraSayed72 <sayedbushra72@gmail.com>
2023-01-20 15:51:32 +0530

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaShoppingCart  } from "react-icons/fa";
 import vector6 from '../Images/Vector (6).png';
 
 
 function Navbar() {
+  const [isNavbarClicked, setIsNavbarClicked] = useState(false)
   return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
     <div className='navbar'>
     <div className='greenFlat'></div>
     <div className='greenCircle'></div>
@@ -12,8 +15,13 @@ function Navbar() {
    <img src={vector6} className='heading'/> 
    <div className='halfsemicircle'></div>
     
-    <div >
-        <ul className='right-nav' >
+      
+    </div>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setIsNavbarClicked(!isNavbarClicked)}>
+      <span className="navbar-toggler-icon"></span>
+    </button>
+   <div className={`${!isNavbarClicked ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+        <ul className='right-nav navbar-nav me-auto mb-2 mb-lg-0' >
             <li className='text-primary'>Shop</li>
             <li className='text-primary'> About us</li>
             <li className='text-primary'>My workbooks</li>
@@ -22,8 +30,8 @@ function Navbar() {
             <p><FaShoppingCart className='nav-icon'/></p>
         </ul>
     </div>
-      
     </div>
+      </nav>
   )
 }
 
